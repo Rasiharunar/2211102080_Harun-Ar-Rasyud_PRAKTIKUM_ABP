5af9d6834f214d8cf66d07b39ce114c9532393c5
Harun Ar Rasyid <ramaeshanin2@gmail.com>
2025-03-03 11:17:39 +0700

    init

---
+++ praktikum_2/lib/variable.dart
@@ -0,0 +1,16 @@
+void main() {
+  int myInt = 22;
+  double myDouble = 20.8;
+  String myString = "Harun Ar Rasyid";
+  bool myBool = false;
+  List<String> myList = ["Sokaraja", "Banyumas", "Brebes"];
+
+  Map<String, int> myMap = {'apple': 1, 'banana': 2};
+
+  print(myInt);
+  print(myDouble);
+  print(myString);
+  print(myBool);
+  print(myList);
+  print(myMap);
+}
